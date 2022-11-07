db8b792fe8cf476a058aeecacc4fd7c1b1c6e4a6
Aziz Limonu <azizlimonu@gmail.com>
2022-11-07 20:45:40 +0800

    initial commit & add navbar

---
+++ client/src/component/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import searchIcon from '../Images/search.png';
+import notifIcon from '../Images/bell.png';
+import messageIcon from '../Images/message.png';
+import profileIcon from '../Images/Profile.png';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+  const handleLogout = () => {
+    console.log('logout triggered');
+  };
+
+  return (
+    <div className='mainNavbar'>
+      <div className='LogoContainer'>
+        <p>Facebook 2.0</p>
+      </div>
+
+      <div>
+        <div className='searchInputContainer'>
+          <img src={searchIcon} alt='search' className='searchIcon' />
+          <input type='text' className='searchInput' placeholder='Search...' name='' id='' />
+        </div>
+      </div>
+
+      <div className='IconsContainer'>
+        <img src={notifIcon} className='Icons' alt='notif' />
+        <img src={messageIcon} className='Icons' alt='message' />
+
+        {/* <Link to={`/profile/${id}`}> */}
+        <Link to='/profile'>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <img src={profileIcon} className='ProfileImage' alt='' />
+            <p style={{ marginLeft: '5px',display:'flex',textAlign:'center' }}>Aziz Limonu</p>
+          </div>
+        </Link>
+
+        <div style={{ marginRight: '30px', marginLeft: '20px', cursor: 'pointer' }} onClick={handleLogout}>
+          <p>Logout</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../component/Navbar/Navbar';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      {/* Navbar */}
+      <Navbar />
+      <div>
+        {/* LeftBar */}
+        {/* MainPost */}
+        {/* RightBar */}
+      </div>
+    </div>
+  )
+}
+
+export default Home
